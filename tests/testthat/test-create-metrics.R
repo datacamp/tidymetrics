1d3f17905525dffdc4f02f1d8c962165b98166d5
David Robinson <admiral.david@gmail.com>
2019-06-17 23:28:49 -0400

    Added more tests of metrics, including printing, filtering, arranging, and condensing them

--- tests/testthat/test-create-metrics.R
+++ tests/testthat/test-create-metrics.R
@@ -2,10 +2,11 @@ context("test-create-metrics")
 
 skip_if_not_installed("nycflights13")
 
-library(dplyr)
+suppressPackageStartupMessages(library(dplyr))
+library(stringr)
 library(nycflights13)
 
-test_that("Can create metrics based on an Rmd", {
+test_that("Can create metrics based on an Rmd, and operate on them", {
   # find flight delays by week, month, and quarter
   # To make it faster, filter for only flights < 10 AM
   flight_summary <- flights %>%
@@ -26,5 +27,40 @@ test_that("Can create metrics based on an Rmd", {
 
     expect_equal(sort(unique(m$period)), c("month", "quarter", "week"))
     expect_gt(nrow(m), 1000)
+
+    # print
+    output <- capture.output(print(m))
+    expect_true(any(stringr::str_detect(output, "Dimensions: origin, carrier")))
+    expect_true(any(stringr::str_detect(output, "Periods:.*month")))
+    expect_true(any(stringr::str_detect(output, "tibble")))
+    expect_true(any(stringr::str_detect(output, "more rows")))
+
+    # dplyr verbs
+    filtered <- m %>%
+      filter(period == "month")
+
+    expect_is(filtered, "tbl_metric")
+    expect_equal(unique(filtered$period), "month")
+    expect_gt(nrow(filtered), 100)
+
+    counted <- m %>%
+      count(date, period)
+
+    expect_is(counted, "tbl_metric")
+    expect_equal(colnames(counted), c("date", "period", "n"))
+
+    arranged <- m %>%
+      arrange(value)
+
+    expect_is(arranged, "tbl_metric")
+    expect_equal(arranged$value, sort(m$value))
+
+    # condense
+    condensed <- m %>%
+      condense_metric(max_dimensions = 1)
+
+    expect_false(any(condensed$origin != "All" & condensed$carrier != "All"))
+    expect_true(any(condensed$origin != "All" & condensed$carrier == "All"))
+    expect_true(any(condensed$origin == "All" & condensed$carrier != "All"))
   }
 })
